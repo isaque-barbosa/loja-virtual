6357a57f80dfd2990234ad4bceb1a942d1cfbf99
IsaqueBarbosa <isaque.barbosa@base2.com.br>
2021-12-03 17:07:59 -0300

    adicionado o axios

--- loja.front/loja-front/src/pages/Home.tsx
+++ loja.front/loja-front/src/pages/Home.tsx
@@ -1,12 +1,43 @@
-import { MainContent } from "../hooks/MainContent";
+import React from "react";
 
-export function Home(){
+import { MainContent } from "../hooks/MainContent";
+import { useFetch } from "../hooks/useFetch";
+
+interface Produto {
+    id: number;
+    nome: string;
+    preco: number;
+};
+
+export const Home: React.FC = () => {
+    //const { data } = useFetch<Produto[]>(`http://localhost:5001/api/produto/detalhes/${id}`);
+    const { data } = useFetch<Produto[]>("api/produto/listar");
+
+    if(!data){
+        return <p>Carregando...</p>
+    }
 
     return(
         <MainContent>
-            <div>
-                <h1>Produtos</h1>
-            </div>
+            <ul>
+                {data.map(produto => (
+                    <li key={produto.id}>
+                        <label>{produto.nome}</label>
+                    </li>
+                ))}
+            </ul>
         </MainContent>
     );
-}
+};
+
+// export function Home(){
+//     const {} = useFetch<User>("http://localhost:5001/api/login");
+
+//     return(
+//         <MainContent>
+//             <div>
+//                 <h1>Produtos</h1>
+//             </div>
+//         </MainContent>
+//     );
+// }
--- loja.front/loja-front/src/pages/Login.tsx
+++ loja.front/loja-front/src/pages/Login.tsx
@@ -1,6 +1,15 @@
-import { MainContent } from "../hooks/MainContent";
+import React from "react";
 
-export function Login(){
+import { MainContent } from "../hooks/MainContent";
+import { useFetch } from "../hooks/useFetch";
+
+interface User {
+    id: number;
+    name: string;
+}
+
+export const LogIn: React.FC = () => {
+    const {} = useFetch<User>("api/login");
 
     return(
         <MainContent>
@@ -15,4 +24,23 @@ export function Login(){
             </div>
         </MainContent>
     );
-}
+};
+
+// export function Login(){
+//     const {} = useFetch<User>("http://localhost:5001/api/login");
+
+
+//     return(
+//         <MainContent>
+//             <div>
+//                 <form>
+//                     <label>E-mail</label>
+//                     <input />
+//                     <label>Senha</label>
+//                     <input />
+//                     <button type="submit">Entrar</button>
+//                 </form>
+//             </div>
+//         </MainContent>
+//     );
+// }
